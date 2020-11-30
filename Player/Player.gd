d6db96223d11206e9b89ae78a567387350c24404
cadelow <70243463+cadelow@users.noreply.github.com>
2020-11-29 21:39:57 -0500

    terraine, targets added

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,12 +1,14 @@
 extends KinematicBody
 
 onready var Camera = get_node("/root/Game/Player/Pivot/Camera")
+onready var Pivot = get_node("/root/Game/Player/Pivot")
 
 var velocity = Vector3()
 var gravity = -9.8
 var speed = 0.2
 var max_speed = 4
 var mouse_sensitivity = 0.002
+var target = null
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -26,12 +28,15 @@ func _physics_process(_delta):
 	velocity.y = falling
 	$AnimationTree.set("parameters/Idle_Run/blend_amount",current_speed/max_speed)
 	velocity = move_and_slide(velocity,Vector3.UP, true)
+	
+	if Input.is_action_just_pressed("shoot") and target!=null and target.is_in_group("target"):
+		target.die()
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		#$Pivot.rotate_x(-event.relative.y * mouse_sensitivity)
 		rotate_y(-event.relative.x * mouse_sensitivity)
-		#$Pivot.rotation.x = clamp($Pivot.rotation.x, -mouse_range, mouse_range)
+		Pivot.rotate_x(event.relative.y * mouse_sensitivity)
+		Pivot.rotation_degrees.x = clamp(Pivot.rotation_degrees.x,-30,15)
 
 func get_input():
 	var input_dir = Vector3()
